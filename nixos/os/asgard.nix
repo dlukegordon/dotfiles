aa4c28e5e84686aeb0ea922ff9ee2a8a87cead83
Lucas Gordon <luke@asgard>
2025-01-27 08:27:31 -0600

    Add intel qsv for asgard

--- nixos/os/asgard.nix
+++ nixos/os/asgard.nix
@@ -25,4 +25,7 @@
 
   # Override default so that we use Alt and instead of Super
   systemd.services.xkeysnail.serviceConfig.ExecStart = "${pkgs.xkeysnail}/bin/xkeysnail /etc/xkeysnail/alt.py";
+
+  # Enable Intel Quick Sync Video
+  hardware.graphics.extraPackages = [pkgs.vpl-gpu-rt];
 }
