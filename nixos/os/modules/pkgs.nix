bf4d9d7f0f863de8d0e7273b76ffbbd9f26fd1bf
luke <luke@asgard>
2025-09-28 16:54:43 -0500

    Add postgres

--- nixos/os/modules/pkgs.nix
+++ nixos/os/modules/pkgs.nix
@@ -111,6 +111,25 @@
   services.blueman.enable = true;
   services.ivpn.enable = true;
 
+  # Postgres
+  services.postgresql = {
+    enable = true;
+    settings.port = 5432;
+    authentication = pkgs.lib.mkOverride 10 ''
+      # Allow local connections without password
+      local all all trust
+      # Allow localhost connections without password
+      host all all 127.0.0.1/32 trust
+      host all all ::1/128 trust
+    '';
+    initialScript = pkgs.writeText "backend-initScript" ''
+      CREATE USER postgres WITH SUPERUSER;
+      ALTER USER postgres PASSWORD 'postgres';
+      CREATE DATABASE defaultdb OWNER postgres;
+    '';
+    extensions = ps: with ps; [ postgis ];
+  };
+
   # Fonts
   fonts.packages = with pkgs; [
     (callPackage ./etbembo.nix { })
